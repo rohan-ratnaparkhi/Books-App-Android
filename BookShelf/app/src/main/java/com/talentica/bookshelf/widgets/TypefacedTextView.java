609828c236095a2a66b2097cf0ceb25e34870a31
rohan-ratnaparkhi <rohanr@talenticaadmins-Mac-mini-3.local>
2016-07-13 12:38:35 +0530

    custom text view widget + roboto fonts added + text sizes to use in app

---
+++ BookShelf/app/src/main/java/com/talentica/bookshelf/widgets/TypefacedTextView.java
@@ -0,0 +1,33 @@
+package com.talentica.bookshelf.widgets;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.talentica.bookshelf.R;
+
+/**
+ * Created by rohanr on 7/13/16.
+ */
+public class TypefacedTextView extends TextView {
+
+    public TypefacedTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        //Typeface.createFromAsset doesn't work in the layout editor. Skipping...
+        if (isInEditMode()) {
+            return;
+        }
+
+        TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.TypefacedTextView);
+        String fontName = styledAttrs.getString(R.styleable.TypefacedTextView_typeface);
+        styledAttrs.recycle();
+
+        if (fontName != null) {
+            Typeface typeface = Typeface.createFromAsset(context.getAssets(), fontName);
+            setTypeface(typeface);
+        }
+    }
+}
