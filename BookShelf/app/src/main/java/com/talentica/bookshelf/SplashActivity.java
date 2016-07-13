388e4d54c214c18b1eea9d7883ffd1a70a019347
rohan-ratnaparkhi <rohanr@talenticaadmins-Mac-mini-3.local>
2016-07-13 11:00:12 +0530

    displaying actual splash screen

--- BookShelf/app/src/main/java/com/talentica/bookshelf/SplashActivity.java
+++ BookShelf/app/src/main/java/com/talentica/bookshelf/SplashActivity.java
@@ -1,18 +1,33 @@
 package com.talentica.bookshelf;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by rohanr on 7/11/16.
  */
 public class SplashActivity extends AppCompatActivity  {
+    Context ctx;
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        ctx = this;
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(ctx, LoginActivity.class);
         startActivity(intent);
         finish();
+
+//        final Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent(ctx, LoginActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        }, 5000);
+
     }
 }
