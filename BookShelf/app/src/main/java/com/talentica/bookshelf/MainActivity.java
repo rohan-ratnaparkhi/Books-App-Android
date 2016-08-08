b770cb1e77e7f940e650e067e7818b3531122609
rohan-ratnaparkhi <rohanr@talenticaadmins-Mac-mini-3.local>
2016-08-08 14:05:40 +0530

    added icons for notifications + add + profile

--- BookShelf/app/src/main/java/com/talentica/bookshelf/MainActivity.java
+++ BookShelf/app/src/main/java/com/talentica/bookshelf/MainActivity.java
@@ -1,74 +1,63 @@
 package com.talentica.bookshelf;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, AHBottomNavigation.OnTabSelectedListener {
-
-    ImageButton bar_home, bar_todo;
-    AHBottomNavigation bottomNavigation;
+    ImageButton nav_home;
+    ImageButton nav_todo;
+    ImageButton nav_add;
+    ImageButton nav_notification;
+    ImageButton nav_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bar_home = (ImageButton) findViewById(R.id.toolbar_home);
-        bar_todo = (ImageButton) findViewById(R.id.toolbar_todo);
-        bar_home.setOnClickListener(this);
-        bar_todo.setOnClickListener(this);
-//        bottomNavigation= (AHBottomNavigation) findViewById(R.id.bottom_navigation);
-//        bottomNavigation.setOnTabSelectedListener(this);
-//        this.createNavItems();
-    }
+        nav_home = (ImageButton) findViewById(R.id.toolbar_home);
+        nav_todo = (ImageButton) findViewById(R.id.toolbar_todo);
+        nav_add = (ImageButton) findViewById(R.id.toolbar_add);
+        nav_notification = (ImageButton) findViewById(R.id.toolbar_notification);
+        nav_profile = (ImageButton) findViewById(R.id.toolbar_profile);
 
-//    private void createNavItems() {
-//        //CREATE ITEMS
-//        AHBottomNavigationItem crimeItem = new AHBottomNavigationItem("", R.drawable.icon_home_select);
-//        AHBottomNavigationItem crimeItems = new AHBottomNavigationItem("", R.drawable.icon_home);
-////        AHBottomNavigationItem dramaItem = new AHBottomNavigationItem("Drama", R.drawable.ac);
-////        AHBottomNavigationItem docstem = new AHBottomNavigationItem("Crime", R.drawable.dr);
-//
-//        //ADD THEM to bar
-//        bottomNavigation.addItem(crimeItem);
-//        bottomNavigation.addItem(crimeItems);
-////        bottomNavigation.
-//        bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#aaaaaa"));
-//        bottomNavigation.setAccentColor(Color.parseColor("#dddddd"));
-//        bottomNavigation.setForceTint(true);
-//        bottomNavigation.setCurrentItem(0);
-//    }
+        nav_home.setOnClickListener(this);
+        nav_todo.setOnClickListener(this);
+        nav_add.setOnClickListener(this);
+        nav_notification.setOnClickListener(this);
+        nav_profile.setOnClickListener(this);
+    }
 
     @Override
     public void onClick(View v) {
+        resetToolbarIconsToDefault();
         switch (v.getId()){
             case R.id.toolbar_home:
-                resetToolbarIconsToDefault();
-                bar_home.setImageResource(R.drawable.icon_home_select);
-                Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+                nav_home.setImageResource(R.drawable.icon_home_select);
                 break;
             case R.id.toolbar_todo:
-                resetToolbarIconsToDefault();
-                bar_todo.setImageResource(R.drawable.icon_todo_select);
-                Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+                nav_todo.setImageResource(R.drawable.icon_todo_select);
+                break;
+            case R.id.toolbar_add:
+                nav_add.setImageResource(R.drawable.icon_add_select);
+                break;
+            case R.id.toolbar_notification:
+                nav_notification.setImageResource(R.drawable.icon_notification_select);
+                break;
+            case R.id.toolbar_profile:
+                nav_profile.setImageResource(R.drawable.icon_profile_select);
                 break;
         }
     }
 
     void resetToolbarIconsToDefault(){
-        bar_home.setImageResource(R.drawable.icon_home);
-        bar_todo.setImageResource(R.drawable.icon_todo);
+        nav_home.setImageResource(R.drawable.icon_home);
+        nav_todo.setImageResource(R.drawable.icon_todo);
+        nav_add.setImageResource(R.drawable.icon_add);
+        nav_notification.setImageResource(R.drawable.icon_notification);
+        nav_profile.setImageResource(R.drawable.icon_profile);
     }
 
-    @Override
-    public boolean onTabSelected(int position, boolean wasSelected) {
-        return false;
-    }
 }
